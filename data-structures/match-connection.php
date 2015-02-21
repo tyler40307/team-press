79d6557a1cbc6f778b150e0b1ce04d5f3c1c1df2
tylercherpak <tyler@functionlabs.io>
2015-02-21 17:11:34 -0500

    fixing indentation

--- data-structures/match-connection.php
+++ data-structures/match-connection.php
@@ -3,10 +3,10 @@
 class Match_Connections {
 
 	public static function init() {
-  	// if ( !function_exists('_p2p_init') )
-  	// 	return _doing_it_wrong( __CLASS__, 'Posts to Posts plugin is required to register connections.', null );
+	if ( !function_exists('_p2p_init') )
+		return _doing_it_wrong( __CLASS__, 'Posts to Posts plugin is required to register connections.', null );
 
- 		add_action( 'p2p_init',  array( __CLASS__, 'register_connections') );
+		add_action( 'p2p_init',  array( __CLASS__, 'register_connections') );
 	}
 
 	public static function register_connections() {
@@ -15,16 +15,16 @@ class Match_Connections {
 		  'from' => 'team',
 		  'to' => 'match',
 		  'admin_box' => array(
-			  'show' => 'any',
-			  'context' => 'side'
+				'show' => 'any',
+				'context' => 'side'
 			),
 			'fields' => array(
-        'count' => array(
-          'title' => 'Count',
-          'type' => 'text'
-        	)
-       	)
-      ) );
+				'count' => array(
+					'title' => 'Count',
+					'type' => 'text'
+				)
+			)
+	  ) );
 	}
 }
 
